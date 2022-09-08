b7f972410f974d084ab20a40dc15605fa0223038
yanz <dirtysalt1987@gmail.com>
2022-09-08 15:52:32 +0800

    add addi/jal/jalr. some rtl primitives are missing

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -2,6 +2,6 @@
 
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi)
+#define INSTR_LIST(f) f(auipc) f(ld) f(sd) f(inv) f(nemu_trap) f(addi) f(jal) f(jalr)
 
 def_all_EXEC_ID();
--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -5,3 +5,19 @@ def_EHelper(auipc) {
 def_EHelper(addi) {
     rtl_addi(s, ddest, id_src1->preg, id_src2->imm);
 }
+
+def_EHelper(jal) {
+    rtl_li(s, ddest, s->pc + 4);
+    word_t imm = (id_src1->imm >> 12);
+    word_t val = (((imm >> 19) & 0x1) << 19) | ((imm & 0xff) << 11) | (((imm >> 8) & 0x1) << 10) | ((imm >> 9) & 0x3ff);
+    word_t ext = SEXT(val << 1, 21);
+    // printf("val = %ld, ext = %ld\n", val << 1, result);
+    rtl_j(s, s->pc + ext);
+}
+
+def_EHelper(jalr) {
+    rtl_li(s, t0, s->pc + 4);
+    // TODO(yan):
+    rtl_addi(s, s0, id_src1->preg, SEXT(id_src2->imm, 12));
+    rtl_jr(s, t0);
+}
