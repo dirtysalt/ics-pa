010ecc0e8a97d5b9f9b258a3a23bf8caf7a278a9
yanz <dirtysalt1987@gmail.com>
2022-09-09 06:21:23 +0800

    rewrite pseudo rtl

--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -8,6 +8,8 @@ def_EHelper(addi) {
 
 def_EHelper(jal) {
     rtl_li(s, ddest, s->pc + 4);
+    // TODO(yan): can we manipulate imm right here?
+    // and can we take s->pc + ext as primitive operation?
     word_t imm = (id_src1->imm >> 12);
     word_t val = (((imm >> 19) & 0x1) << 19) | ((imm & 0xff) << 11) | (((imm >> 8) & 0x1) << 10) | ((imm >> 9) & 0x3ff);
     word_t ext = SEXT(val << 1, 21);
