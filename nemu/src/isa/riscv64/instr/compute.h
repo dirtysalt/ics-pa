c0b0915dfdd18dc16bc3df5795b1a081bf99bbca
yanz <dirtysalt1987@gmail.com>
2022-09-09 16:53:57 +0800

    update files

--- nemu/src/isa/riscv64/instr/compute.h
+++ nemu/src/isa/riscv64/instr/compute.h
@@ -54,19 +54,21 @@ def_EHelper(sltiu) {
 
 #define BRANCH_LOG(name, op)                                                                            \
     Log("pc = " FMT_WORD ", snpc = " FMT_WORD ", imm = " FMT_WORD ", val = " FMT_WORD ", a = " FMT_WORD \
-        ", b = " FMT_WORD ", result = " FMT_WORD,                                                       \
+        ", b = " FMT_WORD ", dnpc = " FMT_WORD,                                                         \
         s->pc, s->snpc, imm, ext, *id_src1->preg, *id_dest->preg, *s0);
 
 #define BRANCH_TEMPLATE(name, op)                              \
     def_EHelper(name) {                                        \
         word_t imm = (id_src2->imm);                           \
         word_t val = BRANCH_SHUFFLE(imm);                      \
-        word_t ext = SEXT(val, 12);                            \
+        word_t ext = SEXT(val << 1, 13);                       \
         rtl_setrelop(s, op, s0, id_src1->preg, id_dest->preg); \
-        rtl_sub(s, s0, rz, s0);                                \
-        rtl_andi(s, s0, s0, ext);                              \
+        rtl_sub(s, s0, rz, s0); /* 0xfffff is 1*/              \
+        rtl_andi(s, s1, s0, s->pc + ext);                      \
+        rtl_not(s, s0, s0); /* 0x0 is 0 */                     \
+        rtl_andi(s, s2, s0, s->snpc);                          \
+        rtl_or(s, s0, s1, s2);                                 \
         BRANCH_LOG(name, op);                                  \
-        rtl_addi(s, s0, s0, s->snpc);                          \
         rtl_jr(s, s0);                                         \
     }
 
