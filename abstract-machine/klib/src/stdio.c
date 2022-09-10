aec7e83f4eba14ebe5d34a74439f7bbb3426689f
yanz <dirtysalt1987@gmail.com>
2022-09-10 13:34:43 +0800

    update filesls

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,104 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+static inline int write_str(char* s, int n, const char* buf) {
+    if (s == NULL) {
+        for (int i = 0; buf[i]; i++) {
+            putch(buf[i]);
+        }
+        return 0;
+    }
+    int i = 0;
+    for (i = 0; i < n && buf[i]; i++) {
+        putch(buf[i]);
+    }
+    return i;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+static inline int write_int(char* s, int n, int d) {
+    char tmp[32];
+    char* buf = tmp + 31;
+    *buf = 0;
+    if (d == 0) {
+        *(--buf) = '0';
+    }
+    while (d != 0) {
+        *(--buf) = '0' + d % 10;
+        d = d / 10;
+    }
+    return write_str(s, n, buf);
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+static inline int write_char(char* s, int n, char c) {
+    if (s == NULL) {
+        putch(c);
+        return 0;
+    }
+    if (n == 0) {
+        return 0;
+    }
+    *s = c;
+    return 1;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char* fmt, ...) {
+    va_list arglist;
+    va_start(arglist, fmt);
+    int ret = vsprintf(NULL, fmt, arglist);
+    va_end(arglist);
+    return ret;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char* out, const char* fmt, va_list ap) {
+    size_t n = 0x7fffffff;
+    int ret = vsnprintf(out, n, fmt, ap);
+    return ret;
+}
+
+int sprintf(char* out, const char* fmt, ...) {
+    va_list arglist;
+    va_start(arglist, fmt);
+    int ret = vsprintf(out, fmt, arglist);
+    va_end(arglist);
+    return ret;
+}
+
+int snprintf(char* out, size_t n, const char* fmt, ...) {
+    // panic("Not implemented");
+    va_list arglist;
+    va_start(arglist, fmt);
+    int ret = vsnprintf(out, n, fmt, arglist);
+    va_end(arglist);
+    return ret;
+}
+
+int vsnprintf(char* out, size_t n, const char* fmt, va_list ap) {
+    int j = 0;
+    for (int i = 0; fmt[i] != 0; i++) {
+        char c = fmt[i];
+        if (c == '%') {
+            c = fmt[i + 1];
+            i++;
+            if (c == 'd') {
+                int val = va_arg(ap, int);
+                j += write_int(out + j, n - j, val);
+            } else if (c == 's') {
+                const char* s = va_arg(ap, const char*);
+                j += write_str(out + j, n - j, s);
+            } else {
+                panic("directive not supported");
+            }
+        } else {
+            j += write_char(out + j, n - j, c);
+        }
+        if (j >= n) return n;
+    }
+    out[j] = 0;
+    return j;
 }
 
 #endif
--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@
 
 export NEMU_HOME=$HOME/repo/ics-pa/nemu
 export AM_HOME=$HOME/repo/ics-pa/abstract-machine
-# export AM_HOME=$HOME/repo/ics-pa/am-kernels
 export ISA="riscv64"
+export ARCH="riscv64-nemu"
