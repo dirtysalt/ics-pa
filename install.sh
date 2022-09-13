cde7d433d90a6bfca34947d16f552541dc8c2dd6
yanz <dirtysalt1987@gmail.com>
2022-09-13 13:55:09 +0800

    update files

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+sudo apt-get install build-essential    # build-essential packages, include binary utilities, gcc, make, and so on
+sudo apt-get install man                # on-line reference manual
+sudo apt-get install gcc-doc            # on-line reference manual for gcc
+sudo apt-get install gdb                # GNU debugger
+sudo apt-get install git                # revision control system
+sudo apt-get install libreadline-dev    # a library used later
+sudo apt-get install libsdl2-dev        # a library used later
+sudo apt-get install llvm-11               # llvm project, which contains libraries used later
 sudo apt-get install g++-riscv64-linux-gnu binutils-riscv64-linux-gnu
