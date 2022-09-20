d67c71279053991c4800404ead8d0790a9afce1d
yanz <dirtysalt1987@gmail.com>
2022-09-20 12:25:14 +0800

    update files

--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@
 
 export NEMU_HOME=$HOME/repo/ics-pa/nemu
 export AM_HOME=$HOME/repo/ics-pa/abstract-machine
+export NAVY_HOME=$HOME/repo/ics-pa/navy-apps
 export ISA="riscv64"
 export ARCH="riscv64-nemu"
